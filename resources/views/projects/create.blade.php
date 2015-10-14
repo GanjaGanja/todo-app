4c777140b42e4dd3cb2e9952d9e8b7e061f13f64
GanjaGanja <ochenj.gorkyj@users.noreply.github.com>
2015-10-14 12:56:46 +0600

    add basic views

---
+++ resources/views/projects/create.blade.php
@@ -0,0 +1,5 @@
+@extends('app')
+
+@section('content')
+	This is my /resoutces/views/projects/create.blade.php file!
+@endsection
---
+++ resources/views/projects/show.blade.php
@@ -0,0 +1,5 @@
+@extends('app')
+
+@section('content')
+	This is my /resoutces/views/projects/show.blade.php file!
+@endsection
