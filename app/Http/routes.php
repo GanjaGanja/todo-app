17eb5dc25100314d9a55aa3675c61276f75094a2
GanjaGanja <ochenj.gorkyj@users.noreply.github.com>
2015-10-17 11:39:06 +0600

    fix links at 'app.blade.php' and 'routes.php'

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,7 +12,8 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return Redirect::route('projects.index');
 });
 
 //Provide controller methods with object instead of ID
